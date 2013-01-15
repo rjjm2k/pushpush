ab4ee450f09e3c6de6ae73d8cad186435c701573
rjjm2k <rjjm2k@gmail.com>
2013-01-15 12:03:41 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pushpush::Application.config.secret_token = '0db39f302fd198d1a8da7646a9a27b79cc209737c9895859b0787c24a05211b8f56c07083efe3f04fd4718855e6284080c6dd1af711d062d41795268b012d0b3'
